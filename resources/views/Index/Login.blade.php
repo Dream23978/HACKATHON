d195f8ef60c208780e0c38883897bc3a59d828f3
Dream23978 <ferdian.ff1@gmail.com>
2025-06-20 18:54:49 +0700

    first

---
+++ resources/views/Index/Login.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+        @vite('resources/css/app.css')
+</head>
+<body class="bg-gradient-to-t from-[#8db9f2c8] to-[#fcfcfc]">
+
+  <img src ="{{ url('img/LOGO_MEADYS.png') }} "height="240" class=" relative -top-14">
+<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 -mt-40 rounded-2xl shadow-xl">
+  <div class="sm:mx-auto sm:w-full sm:max-w-sm -mt-27
+  shadow  shadow-xl/30 scale-75">
+  </div>
+
+      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Login</h2>
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form class="space-y-6" action="{{ route('Login.store') }}" method="POST">
+        @csrf
+      <div>
+        <label for="username" class="block text-sm/6 font-medium text-gray-900 font-sans">Nama Pengguna</label>
+        <div class="mt-2">
+          <input type="text" name="username" id="username"  required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+        </div>
+      </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm/6 font-medium text-gray-900">Kata sandi</label>
+          <div class="text-sm">
+            <a href="{{route('reset')}}" class="font-semibold text-indigo-600 hover:text-indigo-500">Lupa Password?</a>
+          </div>
+        </div>
+        <div class="mt-2">
+          <input type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Masuk</button>
+      </div>
+    </form>
+
+    <p class="mt-10 text-center text-sm/6 text-gray-500">
+      Tidak punya akun?
+      <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Daftar disini</a>
+    </p>
+  </div>
+</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
+use App\http\Controllers\LoginController;
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+    return view('Index.Login');
+})->name('login');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::get('/reset', function () {
+    return view('reset');
+})->name('reset');
+// Route::get('/login',[LoginController::class,'create']);
+Route::post('/Login/store',[LoginController::class,'store'])->name('Login.store');
+Route::get('/Login/create',[LoginController::class,'create'])->name('masuk');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+// Route::view('dashboard', 'dashboard')
+//     ->middleware(['auth', 'verified'])
+//     ->name('dashboard');
 
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});
+// Route::middleware(['auth'])->group(function () {
+//     Route::redirect('settings', 'settings/profile');
+
+//     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+//     Volt::route('settings/password', 'settings.password')->name('settings.password');
+//     Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+// });
 
 require __DIR__.'/auth.php';
